8df35fa0830ae8dfd0057cd5662c3a8daca19335
hyejin <hyejin_one@naver.com>
2018-10-01 17:55:55 +0900

    start

---
+++ BitMaster/urls.py
@@ -0,0 +1,35 @@
+"""BitMaster URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from main import views as main_views
+from django.contrib import admin
+from django.urls import path
+import user.views as user_view
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', main_views.index),
+
+    # user
+    path('user/joinform/', user_view.joinform),
+    path('user/join', user_view.join),
+    path('user/joinsuccess/', user_view.joinsuccess),
+    path('user/loginform/', user_view.loginform),
+    path('user/login', user_view.login),
+    path('user/logout', user_view.logout),
+
+]
---
+++ user/models.py
@@ -0,0 +1,11 @@
+# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=32)
+    name = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'user(%s, %s, %s, %s)' % (self.email, self.password, self.name, self.gender)
